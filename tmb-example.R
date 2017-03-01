4a2060e109fb18486d5c65796f1ad7b030785a0f
Kyle Chezik <kchezik@gmail.com>
2017-03-01 15:21:15 -0800

    Plot all the climate data individually.

--- tmb-example.R
+++ tmb-example.R
@@ -41,16 +41,29 @@ rep
 # Replicate with Fraser Data.
 library(tidyverse)
 load("out_doy2.RData")
+load("out_med.RData")
+load("out_max.RData")
+load("out_min.RData")
+
+x = out_med
+
+y = x$Area$real_slopes%>% select(8:ncol(.)) %>% apply(.,2,function(x) zero_one(x)) %>% 
+	apply(.,1,function(x) sum(x)) %>% mutate(x$Area$real_slopes, clim.sd = .) %>%  gather(., climate, index, emt.sd,ext.sd,map.sd,mat.sd,pas.sd,clim.sd)
+
+ggplot(y, aes(index, slope)) +
+	geom_point() +
+	facet_wrap(~climate, scales = "free_x")
 
 #Function for scaling values between 0 and 1.
 zero_one = function(x) ((x-min(x)))/(diff(range(x)))
 #Function for standardizing values.
 std = function(x) (x/sd(x))
 
-u = out_doy2$Area$real_slopes$slope
-s1 = out_doy2$Area$real_slopes %>% mutate(STDsqrtArea = std(sqrt(Area))) %>% .$STDsqrtArea
-s2 = out_doy2$Area$real_slopes %>% select(8:ncol(.)) %>% apply(.,2,function(x) zero_one(x)) %>% 
+u = x$Area$real_slopes$slope
+s1 = x$Area$real_slopes %>% mutate(STDsqrtArea = std(sqrt(Area))) %>% .$STDsqrtArea
+s2 = x$Area$real_slopes %>% select(8:ncol(.)) %>% apply(.,2,function(x) zero_one(x)) %>% 
 	apply(.,1,function(x) sum(x)) %>% std(.)
+s2 = x$Area$real_slopes$mat.sd %>% std(.)
 mm = model.matrix(~ s1 + s2)
 
 library(TMB)
@@ -81,5 +94,5 @@ rep
 # Maximum gradient component: 0.02134764
 
 library(nlme)
-mod = gls(slope~s1 + s2, data = out_doy2$Area$real_slopes, weights = varComb(varExp(form = ~s1), varExp(form = ~s2)))
-mod = gls(slope~s1 + s2, data = out_doy2$Area$real_slopes, weights = varExp(form = ~s2))
+mod = gls(slope~s1 + s2, data = x$Area$real_slopes, weights = varComb(varExp(form = ~s1), varExp(form = ~s2)))
+mod = gls(slope~s1 + s2, data = x$Area$real_slopes, weights = varExp(form = ~s2))
