08540dc03588c912323fcc0a6f895e3e13dd6b01
Kyle Chezik <kchezik@gmail.com>
2017-03-07 12:06:26 -0800

    Retool plot code to incorporate new attenPlot library.

--- Code_06 Annual_Funnel_Plot.R
+++ Code_06 Annual_Funnel_Plot.R
@@ -2,116 +2,36 @@
 ################## Plot Annual Flow Trends Against Area and Display Climate Index Data ##################
 #########################################################################################################
 
-#Function for scaling values between 0 and 1.
-zero_one = function(x) ((x-min(x)))/(diff(range(x)))
+library(attenPlot) #Funnel and Density plot function library.
 
-#Function for simplifying data and adding climate index values.
-arrange.data = function(df, response){
-	tp = df[[grep(response, names(df))]]$real_slopes
-	tp = tp %>% select(8:ncol(tp)) %>% apply(.,2,function(x) zero_one(x)) %>% 
-		apply(.,1,function(x) sum(x)) %>% mutate(tp, std.clim = .)
-	names(tp)[grep(response,names(tp))] = "resp"
-	tp %>% mutate(sqrt.resp = sqrt(resp)) %>% mutate(se.upper = slope+se, se.lower = slope-se) %>% arrange(resp)
-}
-
-#Add colour gradient to climate index.
-color.gradient = function(df, pal = "GnBu", shade = "99"){
-	#vector scaled 0-1 for function
-	col = zero_one(df$std.clim)
-	#color ramp function
-	RdYlBu = brewer.pal(9, pal)[3:9]
-	FUN = colorRamp(RdYlBu, bias=1)
-	#apply function
-	cols = FUN(col)
-	cols = rgb(cols, maxColorValue=256)
-	paste(cols, shade, sep = "")
-}
-
-#Axis label determination
-axis.Fun = function(df, yaxis.DOY=F){
-	if(yaxis.DOY == T) {
-		ylimits <<- pretty(c(min(df$slope), max(df$slope)))
-		label <<- round((ylimits/4*38*365)/3.8,0)
-	} else {
-		ylimits <<- pretty(c(min(df$se.lower), max(df$se.upper)))
-		label <<- round(exp(ylimits*10)*100-100,0)
-	}
-}
-
-#Draw x and y axis.
-axis.Draw = function(LW, ALS, ALX, ALY, LL, YLim, YLab, xAxis = T){
-	if(xAxis == T){
-		axis(1, lwd = LW, cex.axis = ALS, outer = T, hadj = 1,
-				 labels = c("0","10000","40000","90000","160000","250000"), at = c(0,100,200,300,400,500))
-		mtext(ALX, side = 1, line = LL, cex = ALS) # x-axis label.
-	} else axis(1, lwd = LW, tick = TRUE, labels = FALSE, tck = 0)
-	axis(2, lwd = LW, cex.axis = ALS, at = YLim, labels = YLab)
-	mtext(ALY, side=2, line=LL, cex = ALS, las = 0)  #y-axis label.
-}
-
-#Add points and SE to plotting region.
-points.SE = function(df, lcol, cols, LW, shade = "99"){
-	segments(df$sqrt.resp, df$slope+df$se, df$sqrt.resp, df$slope-df$se,
-					 col = paste(lcol, shade, sep = ""), lwd = LW)
-	segments(0,0,500,0, lty = 2, lwd = LW)
-	points(df$sqrt.resp, df$slope, col = cols, pch = 16, cex = 1.5)
-}
-
-#Add Simulation Lines to Plotting Region.
-line.sims = function(df, col, LW, shade = "10", transp = T, intercept, slope, resp.sqrt, sigma, varexp){
-	apply(df,1,function(y){
-	if(transp == T) colur = paste(col,shade,sep="")
-	else colur = col
-	upper = intercept + slope*(resp.sqrt - mean(resp.sqrt)) + 1.96 * sqrt(sigma^2 * exp(2*(resp.sqrt/1e3)*y[1]))
-	lines(resp.sqrt, upper, col = colur, lwd = LW)
-	lower = intercept + slope*(resp.sqrt - mean(resp.sqrt)) - 1.96 * sqrt(sigma^2 * exp(2*(resp.sqrt/1e3)*y[1]))
-	lines(resp.sqrt, lower, col = colur, lwd = LW)
-	})}
-
-#Density Plot
-densityP = function(Rvarexp, Svarexp, d.axis=F, LW, ALS, Lab.Loc,	
-										shade, col1, col2, col3, col4, vline, pane){
-	d = density(c(Svarexp,Rvarexp), bw = "SJ")
-	df = tbl_df(data.frame(varexp = d$x, dens = d$y))
+#Density Plot Wrapper
+densityP = function(df, response, type = "exp", d.axis=F, LW=L.Width, ALS=Axis.Lab.Size, LL,	
+										shade = "99", col1, col2, col3, col4, vline, pane){
+	
+	gather.density.data(df, response, type)
+	d_dat = dplyr::tbl_df(data.frame(var = d$x, dens = d$y))
+	
 	par(mar = c(0,0.3,1.5,0.5), mgp = c(2,0.60,0))
-	plot(dens~varexp, data = df, type = "n", xlab = "", ylab = "", las = 1,
+	plot(dens~var, data = d_dat, type = "n", xlab = "", ylab = "", las = 1,
 			 bty = "n", yaxt = "n", xaxt = "n", axes = F, xaxs = "i", xlim = c(-12,5))
 	
 	if(d.axis == TRUE){
-		mtext(expression("Var. Exp. Param. ("*delta*")"), side = 1, line = Lab.Loc, cex = ALS - 0.2) # x-axis label.	
-		axis(1, lwd = LW, tck = -0.04, cex.axis = ALS, outer = T)
+		axis.density.draw(type = type, ALS = ALS, LL = LL, LW = LW, annual = T)
 	} else axis(1, lwd = LW, labels = F, tck = 0)
 	
-	Upper = filter(df, varexp>Rvarexp)
-	Upper = rbind(Upper, c(varexp = Rvarexp, dens = 0))
-	Lower = filter(df, varexp<Rvarexp)
-	Lower = rbind(Lower, c(varexp = Rvarexp, dens = 0))
+	plygnPrptns(d_dat, type, annual = T, varR, varS)
 	
-	if(is.null(Upper$varexp)==F){
-		polygon(Upper$varexp, Upper$dens, col=paste(col1, shade, sep=""),
-						border=paste(col3, shade, sep=""))
-		prop = round(sum(Rvarexp<Svarexp)/length(Svarexp),2)
-		text(2,max(df$dens)/2,
-				 labels = as.character(prop), cex = ALS)
-	}
-	if(is.null(Lower$varexp)==F){
-		polygon(Lower$varexp, Lower$dens, col=paste(col3, shade, sep=""),
-						border=paste(col4, shade, sep=""))
-		text(-8,max(df$dens)/2,
-				 labels = as.character(round(1-prop,2)), cex = ALS)
-	}
-	abline(v = Rvarexp, col = paste(col2, shade, sep=""), lwd = vline)
-	abline(v = Rvarexp, col = paste(col2, shade, sep=""), lwd = vline)
-	text(4, max(df$dens), labels = pane, pos = 2, cex = ALS)
+	for(i in 1:2) abline(v = varR, col = paste(col2, shade, sep=""), lwd = vline)
+	text(4, max(d_dat$dens), labels = pane, pos = 2, cex = ALS)
 }
 
 
-# Funnel Plot Wrapper.
-funnel.wrapper = function(dat, response, yaxis.DOY = F, xAxis = T, pane, axis.Lx=NULL, axis.Ly=NULL, d.axis = F, iter=1000){
+#Funnel Plot Wrapper.
+funnel.wrapper = function(dat, response, yaxis.DOY = F, xAxis = T, pane, axis.Ly=NULL, d.axis = F, iter=1000){
 	
 	############################################ Prepare Data ###############################################
 	#Control Panel
-	L.Width = 1; Axis.Lab.Size = 1.1; Lab.Loc = 3.1; vLine = 4
+	L.Width = 1; Axis.Lab.Size = 1.1; LabLoc = 3.1; vLine = 4
 	#Colour Control Panel
 	col1="#F2AD00"; col2="#67A9CF"; col3="#F98400"; col4="#FF0000"; lcol="#969696"
 	#Libraries to load.
@@ -119,7 +39,7 @@ funnel.wrapper = function(dat, response, yaxis.DOY = F, xAxis = T, pane, axis.Lx
 	#Cleanup and arrange observed flow and climate trend data.
 	flow.trends = arrange.data(dat,response)
 	#Create a colour gradient for the climate index values.
-	cols = color.gradient(flow.trends)
+	cols = color.gradient(flow.trends$std.clim)
 	
 	########################################### Plot Raw Data ##############################################
 	#Set initial plotting parameters
@@ -131,10 +51,10 @@ funnel.wrapper = function(dat, response, yaxis.DOY = F, xAxis = T, pane, axis.Lx
 	#Make plotting visible.
 	par(fg = "black")
 	#Draw x and y axis.
-	axis.Draw(LW = L.Width, ALS = Axis.Lab.Size, ALX = axis.Lx, ALY = axis.Ly, 
-						LL = Lab.Loc, YLim = ylimits, YLab = label, xAxis = xAxis)
+	axis.Draw(LW = L.Width, ALS = Axis.Lab.Size, ALY = axis.Ly, 
+						LL = LabLoc, YLim = ylimits, YLab = label, xAxis = xAxis)
 	#Add points and SE to Plotting Region.
-	points.SE(flow.trends, lcol, cols, L.Width)
+	pointsSE(flow.trends, lcol, cols, L.Width)
 	#Create Label For Each Panel.
 	text(500, max(ylimits), labels = pane, pos = 2, cex = Axis.Lab.Size)
 	
@@ -150,23 +70,21 @@ funnel.wrapper = function(dat, response, yaxis.DOY = F, xAxis = T, pane, axis.Lx
 		#Draw lines.
 			#Less extreme values.
 	line.sims(df = ld.up, col = col1, LW = L.Width, shade = "10", 
-						intercept = intercept, slope = slope, sigma = sigma, varexp = Rvarexp,
+						intercept = intercept, slope = slope, sigma = sigma,
 						resp.sqrt = flow.trends$sqrt.resp)
 			#More extreme values.
 	line.sims(df = ld.low, col = col3, LW = L.Width, shade = "10", 
-						intercept = intercept, slope = slope, sigma = sigma, varexp = Rvarexp,
+						intercept = intercept, slope = slope, sigma = sigma,
 						resp.sqrt = flow.trends$sqrt.resp)
 			#Actual values repeated twice.
 	for(i in c(1:2)) {
 		line.sims(df = real, col = col2, LW = L.Width + 3.5, shade = "99", 
-							intercept = intercept, slope = slope, sigma = sigma, varexp = Rvarexp,
+							intercept = intercept, slope = slope, sigma = sigma,
 							resp.sqrt = flow.trends$sqrt.resp)
 	}
 	
 	######################################## Density Plot ##################################################
-	densityP(Rvarexp = Rvarexp, Svarexp = sims$varexp, 
-					 d.axis = d.axis, LW = L.Width, ALS = Axis.Lab.Size, Lab.Loc = Lab.Loc,
-					 shade = "99", col1 = col1, col2 = col2, col3 = col3, col4 = col4, vline = vLine, pane = pane)
+	densityP(df = dat, response = response, d.axis = d.axis, LW = L.Width, ALS = Axis.Lab.Size, LL = LabLoc, vline = vLine, col2 = col2, pane = pane)
 }
 
 load("out_doy2.RData")
@@ -174,17 +92,15 @@ load("out_min.RData")
 load("out_max.RData")
 load("out_med.RData")
 
-pdf("Fig3_Annual-Funnel.pdf", width = 11, height = 8.5)
-layout(rbind(c(1,1,3,3,2),c(1,1,3,3,4),c(5,5,7,7,6),c(5,5,7,7,8)))
+#pdf("Fig3_Annual-Funnel.pdf", width = 11, height = 8.5)
+annualScreen()
 par(oma = c(4,0,0,0))
 funnel.wrapper(out_doy2, "Area", pane = "a", xAxis = F, yaxis.DOY = T,  
 							 axis.Ly = expression("DOY"~scriptstyle(frac(1,2))~"Annual Flow | Days"%.%"Decade"^-1))
 funnel.wrapper(out_min, "Area", pane = "b", xAxis = F, 
 							 axis.Ly = expression("Minimum-Flow | %Change"%.%"Decade"^-1))
 funnel.wrapper(out_max, "Area", pane = "c",
-							 axis.Lx = expression("Area (km"^2*")"),
 							 axis.Ly = expression("Maximum-Flow | %Change"%.%"Decade"^-1))
 funnel.wrapper(out_med, "Area", pane = "d", d.axis = T,
-							 axis.Lx = expression("Area (km"^2*")"),
 							 axis.Ly = expression("Median-Flow | %Change"%.%"Decade"^-1))
-dev.off()
+#dev.off()
--- Code_07 Monthly_Plot.R
+++ Code_07 Monthly_Plot.R
@@ -1,135 +1,10 @@
-###########################################################################################################
-######################################## Sub-Functions ####################################################
-###########################################################################################################
+#########################################################################################################
+################## Plot Monthly Flow Trends Against Area and Display Climate Index Data #################
+#########################################################################################################
 
+library(attenPlot)#Funnel and Density plot function library.
 
-############################################ Funnel Plot Functions ###############################################
-
-#Function for scaling values between 0 and 1.
-zero_one = function(x) ((x-min(x)))/(diff(range(x)))
-
-#Function for simplifying data and adding climate index values.
-arrange.data = function(df, response){
-	tp = df[[grep(response, names(df))]]$real_slopes
-	tp = tp %>% select(8:ncol(tp)) %>% apply(.,2,function(x) zero_one(x)) %>% 
-		apply(.,1,function(x) sum(x)) %>% mutate(tp, std.clim = .)
-	names(tp)[grep(response,names(tp))] = "resp"
-	tp %>% mutate(sqrt.resp = sqrt(resp)) %>% mutate(se.upper = slope+se, se.lower = slope-se) %>% arrange(resp)
-}
-
-#Add colour gradient to climate index.
-color.gradient = function(df, pal = "GnBu", shade = "99"){
-	#vector scaled 0-1 for function
-	col = zero_one(df$std.clim)
-	#color ramp function
-	RdYlBu = brewer.pal(9, pal)[3:9]
-	FUN = colorRamp(RdYlBu, bias=1)
-	#apply function
-	cols = FUN(col)
-	cols = rgb(cols, maxColorValue=256)
-	paste(cols, shade, sep = "")
-}
-
-#Axis label determination
-axis.Fun = function(df, yaxis.DOY=F, ylimit=NULL){
-	if(yaxis.DOY == T) {
-		ylimits <<- pretty(c(min(df$slope), max(df$slope)))
-		label <<- round((ylimits/4*38*365)/3.8,0)
-	} else {
-		ylimits <<- pretty(c(min(df$se.lower), max(df$se.upper)))
-		label <<- round(exp(ylimits*10)*100-100,0)
-	}
-	if(is.null(ylimit)==F){
-		ylimits <<- pretty(ylimit)
-		label <<- round(exp(ylimits*10)*100-100,0)
-	}
-}
-
-#Draw x and y axis.
-axis.Draw = function(LW, ALS, ALY, LL, YLim, YLab, xAxis = F){
-	if(xAxis == T){
-		axis(1, lwd = LW, cex.axis = ALS, outer = T, hadj = 1,
-				 labels = c("0","10000","40000","90000","160000","250000"), at = c(0,100,200,300,400,500))
-		mtext(expression("Area (km"^2*")"), side = 1, line = LL, cex = ALS) # x-axis label.
-	} else axis(1, lwd = LW, tick = TRUE, labels = FALSE, tck = 0)
-	axis(2, lwd = LW, cex.axis = ALS, at = YLim, labels = YLab)
-	mtext(ALY, side=2, line=LL, cex = ALS, las = 0)  #y-axis label.
-}
-
-#Add points and SE to plotting region.
-points.SE = function(df, lcol, cols, LW, shade = "99"){
-	segments(df$sqrt.resp, df$slope+df$se, df$sqrt.resp, df$slope-df$se,
-					 col = paste(lcol, shade, sep = ""), lwd = LW)
-	segments(0,0,500,0, lty = 2, lwd = LW)
-	points(df$sqrt.resp, df$slope, col = cols, pch = 16, cex = 1.5)
-}
-
-#Add Simulation Lines to Plotting Region.
-line.sims = function(df, col, LW, shade = "10", transp = T, intercept, slope, resp.sqrt, sigma, varexp){
-	apply(df,1,function(y){
-		if(transp == T) colur = paste(col,shade,sep="")
-		else colur = col
-		upper = intercept + slope*(resp.sqrt - mean(resp.sqrt)) + 1.96 * sqrt(sigma^2 * exp(2*(resp.sqrt/1e3)*y[1]))
-		lines(resp.sqrt, upper, col = colur, lwd = LW)
-		lower = intercept + slope*(resp.sqrt - mean(resp.sqrt)) - 1.96 * sqrt(sigma^2 * exp(2*(resp.sqrt/1e3)*y[1]))
-		lines(resp.sqrt, lower, col = colur, lwd = LW)
-	})}
-
-############################################ Density Plot Functions ###############################################
-
-#Gather and arrange observed and simulated variance and intercept parameter estimates.
-gather.density.data = function(df, response, type){
-	tp = df[[grep(response, names(df))]]
-	Rvarexp = tp$real_varexp$varexp 
-	Svarexp = tp$sim_varexp$varexp
-	Rint = tp$real_varexp$intercept
-	Sint = tp$sim_varexp$intercept
-	
-	if(type == "int"){
-		d <<- density(Sint)
-		var.real <<- Rint}
-	else{d <<- density(Svarexp)
-	var.real <<- Rvarexp}
-	varR <<- Rvarexp
-	varS <<- Svarexp
-}
-
-#Plot proportional plots of the variance parameter depicting the simulated values greather than and less than the observed.
-plygnPrptns = function(df, type, varR, varS, col1, col3, col4, shade = "99"){
-	if(type != "int"){
-		Upper = filter(df, var>varR) %>% bind_rows(., data.frame(var = varR, dens = 0))
-		Lower = filter(df, var<varR) %>% bind_rows(., data.frame(var = varR, dens = 0))
-		if(is.null(Upper)==F & nrow(Upper)>1)	polygon(Upper$var,Upper$dens, 
-																									col=paste(col1,shade,sep=""),
-																									border=paste(col1,shade,sep=""))
-		if(is.null(Upper)==F & nrow(Lower)>1) polygon(Lower$var,Lower$dens, 
-																									col=paste(col3,shade,sep=""),
-																									border=paste(col4,shade,sep=""))
-		prop = round(sum(varR<varS)/length(varS),2)
-		mtext(prop, side = 3, adj = 0.05, line = -1.3, cex = 0.75)
-	}
-}
-
-#Draw density plot axis' and labels.
-axis.density.draw = function(type, ALS, Lab.Loc){
-	if(type == "int"){
-		label = round(exp(c(-0.008,-0.004,0,0.004,0.008,0.012,0.016)*10)*100-100,0)
-		axis(1, lwd = 0.5, cex.axis = ALS, outer = T, hadj = 1, labels = label, at = c(-0.008,-0.004,0,0.004,0.008,0.012,0.016))
-		mtext(expression("Intercept | %Change"%.%"Decade"^-1), side = 1, line = Lab.Loc, cex = ALS)
-	} else{
-		axis(1, lwd = 0.5, cex.axis = ALS, outer = T, hadj = 0.5, labels = c("-12","-8","-4","0","4"), at = c(-12,-8,-4,0,4), cex.axis = ALS)
-		mtext(expression("Var. Exp. Param. ("*delta*")"), side = 1, line = Lab.Loc, cex = ALS)	
-	}
-}
-
-
-
-
-###########################################################################################################
-############################################### Wrappers ##################################################
-###########################################################################################################
-
-
+#Density Plot Wrapper
 density_plot = function(data, response, type, xlimit){
 	
 	############################################ Prepare Data ###############################################
@@ -159,7 +34,7 @@ density_plot = function(data, response, type, xlimit){
 		#Add month label if plotting intercept (i.e., far left on page) data.
 		if(type == "int") mtext(Months[count],side=2, line = 2, las = 1, adj = 0); count <<- count+1
 		#If plotting variance exponent parameter data, then provide proportions and polygons.
-		plygnPrptns(d_dat, type, varR, varS, col1, col3, col4)
+		plygnPrptns(d_dat, type, varR, varS, annual = F, col1, col2, col3, col4)
 		#Add vertical line for the real int/exp value.
 		for(i in 1:2){abline(v = var.real ,col = paste(col2,shade,sep=""), lwd = vLine)}
 		#Add dotted vertical line at zero.
@@ -171,8 +46,7 @@ density_plot = function(data, response, type, xlimit){
 	axis.density.draw(type, Axis.Lab.Size, Lab.Loc)
 }
 
-
-
+#Funnel Plot Wrapper.
 funnel.plots = function(dat, response, yaxis.DOY = F, d.xaxis = F, pane, funnel.y = "", axis.Ly = "", xAxis = F, ylimit = NULL, iter = 200){
 	library(tidyverse);library(wesanderson)
 	
@@ -186,7 +60,7 @@ funnel.plots = function(dat, response, yaxis.DOY = F, d.xaxis = F, pane, funnel.
 	#Cleanup and arrange observed flow and climate trend data.
 	flow.trends = arrange.data(dat,response)
 	#Create a colour gradient for the climate index values.
-	cols = color.gradient(flow.trends)
+	cols = color.gradient(flow.trends$std.clim)
 	
 	########################################### Plot Raw Data ##############################################
 	
@@ -202,7 +76,7 @@ funnel.plots = function(dat, response, yaxis.DOY = F, d.xaxis = F, pane, funnel.
 	axis.Draw(LW = L.Width, ALS = Axis.Lab.Size, ALY = axis.Ly, 
 						LL = Lab.Loc, YLim = ylimits, YLab = label, xAxis = xAxis)
 	#Add points and SE to Plotting Region.
-	points.SE(flow.trends, lcol, cols, L.Width)
+	pointsSE(flow.trends, lcol, cols, L.Width)
 	#Create Label For Each Panel.
 	text(500, max(ylimits), labels = pane, pos = 2, cex = Axis.Lab.Size)
 	mtext(funnel.y, side=2, at = 0.05, line=Lab.Loc, cex = Axis.Lab.Size, las = 0)  #y-axis label.
@@ -219,25 +93,23 @@ funnel.plots = function(dat, response, yaxis.DOY = F, d.xaxis = F, pane, funnel.
 	#Draw lines.
 	#Less extreme values.
 	line.sims(df = ld.up, col = col1, LW = L.Width, shade = "10", 
-						intercept = intercept, slope = slope, sigma = sigma, varexp = Rvarexp,
+						intercept = intercept, slope = slope, sigma = sigma,
 						resp.sqrt = flow.trends$sqrt.resp)
 	#More extreme values.
 	line.sims(df = ld.low, col = col3, LW = L.Width, shade = "10", 
-						intercept = intercept, slope = slope, sigma = sigma, varexp = Rvarexp,
+						intercept = intercept, slope = slope, sigma = sigma,
 						resp.sqrt = flow.trends$sqrt.resp)
 	#Actual values repeated twice.
 	for(i in c(1:2)) {
 		line.sims(df = real, col = col2, LW = L.Width + 3.5, shade = "99", 
-							intercept = intercept, slope = slope, sigma = sigma, varexp = Rvarexp,
+							intercept = intercept, slope = slope, sigma = sigma,
 							resp.sqrt = flow.trends$sqrt.resp)
 	}
 }
-#Split up the display for two columns of twelve density plots and one column of 4 funnel plots.
-screen = function(){layout(cbind(c(1:12),c(1:12),c(1:12),c(1:12),c(13:24),c(13:24),c(13:24),c(13:24),c(rep(25,3),rep(26,3),rep(27,3),rep(28,3)),c(rep(25,3),rep(26,3),rep(27,3),rep(28,3)),c(rep(25,3),rep(26,3),rep(27,3),rep(28,3)),c(rep(25,3),rep(26,3),rep(27,3),rep(28,3)),c(rep(25,3),rep(26,3),rep(27,3),rep(28,3))))}
 
 load("max_month.RData")
 #pdf("Fig4_Max-Monthly-Density-Funnel.pdf", width = 11, height = 8.5)
-screen()
+monthlyScreen()
 density_plot(out_max_month, "Area", type = "int", xlimit = c(-0.008,0.017))
 density_plot(out_max_month, "Area", type = "exp", xlimit = c(-12,4))
 funnel.plots(dat = out_max_month$`2`, response = "Area", yaxis.DOY = F, xAxis = F, d.xaxis = F, pane = "February", axis.Ly = NULL, ylimit = c(-0.03,0.025), iter = 500)
@@ -248,7 +120,7 @@ funnel.plots(dat = out_max_month$`11`, response = "Area", yaxis.DOY = F, xAxis =
 
 load("min_month.RData")
 #pdf("Fig4_Min-Monthly-Density-Funnel.pdf", width = 11, height = 8.5)
-screen()
+monthlyScreen()
 density_plot(out_min_month, "Area", type = "int", xlimit = c(-0.008,0.017))
 density_plot(out_min_month, "Area", type = "exp", xlimit = c(-12,4))
 funnel.plots(dat = out_min_month$`2`, response = "Area", yaxis.DOY = F, xAxis = F, d.xaxis = F, pane = "February", axis.Ly = NULL, ylimit = c(-0.03,0.025), iter = 500)
@@ -259,7 +131,7 @@ funnel.plots(dat = out_min_month$`11`, response = "Area", yaxis.DOY = F, xAxis =
 
 load("med_month.RData")
 #pdf("Fig4_Med-Monthly-Density-Funnel.pdf", width = 11, height = 8.5)
-screen()
+monthlyScreen()
 density_plot(out_med_month, "Area", type = "int", xlimit = c(-0.008,0.017))
 density_plot(out_med_month, "Area", type = "exp", xlimit = c(-12,4))
 funnel.plots(dat = out_med_month$`2`, response = "Area", yaxis.DOY = F, xAxis = F, d.xaxis = F, pane = "February", axis.Ly = NULL, ylimit = c(-0.025,0.03), iter = 500)
